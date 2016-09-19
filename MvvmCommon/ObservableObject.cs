4bdbae09508f299aa1016225a7699b1911cea61d
PhB <philbo96@gmail.com>
2016-09-19 19:21:56 +0200

    Project architecture created

---
+++ MvvmCommon/ObservableObject.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace MvvmCommon
+{
+    // Die ursprüngliche Implementierung von ObservableObject kommt aus
+    // Prism (Microsoft.Practices.Composite.Presentation)
+    // http://stackoverflow.com/a/10093257/33311
+    //
+    // Dies ist eine Variante davon.
+    [Serializable]
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Weil die Klasse ViewModel von INotifyPropertyChanged erbt, weiss die WPF, dass 
+        /// die Klasse ein PropertyChangedEventHandler mit dem Namen PropertyChanged gibt. 
+        /// Daher muss das Objekt PropertyChanged heissen.
+        /// Die WPF registriet sich auf diesem Event automatisch.
+        /// </summary>
+        [field: NonSerialized]
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Dank CallerMemverName, weiss die WPF, welches Property die
+        /// Methode aufgerufen hat und gibt das Property der Methode mit. Die Methode liest dann
+        /// die neuen Daten aus dem sich geänderten Property aus und aktuallisiert das GUI.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            this.PropertyChanged?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// Wird manuell ausgeführt und führt dann OnPropertyChanged aus.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            VerifyPropertyName(propertyName);
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Warnt den Compiler, falls es zum spezifizierten Namen kein Property im Objekt existiert.
+        /// Diese Methode ist nur für Debug Build.
+        /// </summary>
+        [Conditional("DEBUG")]
+        [DebuggerStepThrough]
+        public void VerifyPropertyName(string propertyName)
+        {
+            // Sicherstellen, dass der Proterty Name auf dem Objekt exisiert  
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+            {
+                Debug.Fail("Invalid property name: " + propertyName);
+            }
+        }
+    }
+}
---
+++ MvvmCommon/RelayCommand.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MvvmCommon
+{
+    public class RelayCommand : ICommand
+    {
+        /// <summary>
+        /// Dieses Event wird von der WPF aufgerufen. Der CommandManager weiss, wann er das CanExecute ausführen muss.
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            // https://joshsmithonwpf.wordpress.com/2008/06/17/allowing-commandmanager-to-query-your-icommand-objects/
+            // http://stackoverflow.com/questions/6634777/what-is-the-actual-task-of-canexecutechanged-and-commandmanager-requerysuggested
+
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        // Action sowie Func sind Delegaten, welche Methoden mit bestimmter Signatur erwarten.
+        private readonly Action methodToExecute;
+        private readonly Func<bool> canExecuteEvaluator;
+
+        /// <summary>
+        /// Wir geben dem Konstruktor die Methode welche ausgeführt werden soll, sowie die Methode
+        /// um herauszufinden, ob canExecute true oder false sein soll.
+        /// 
+        /// Bsp: RelayCommand relayCommand = new RelayCommand(auszufuehrendeMethode, ueberpruefendeMethode);
+        /// </summary>
+        /// <param name="methodToExecute">Diese Methode wird ausgeführt</param>
+        /// <param name="canExecuteEvaluator">Diese Methode prüft ob canExecute true oder flase ist</param>
+        public RelayCommand(Action methodToExecute, Func<bool> canExecuteEvaluator) 
+        {
+            this.methodToExecute = methodToExecute;
+            this.canExecuteEvaluator = canExecuteEvaluator;
+        }
+
+        /// <summary>
+        /// Der zweite Konstruktor ist dafür, wenn man keine Methode zur überprüfung für CanExecute mitgegeben hat.
+        /// In diesem Fall wird der normale Konstruktor aufgerufen und es wird null als prüfende Methode mitgegeben.
+        /// </summary>
+        /// <param name="methodToExecute"></param>
+        public RelayCommand(Action methodToExecute) : this(methodToExecute, null)
+        {
+            // empty
+        }
+
+        /// <summary>
+        /// Diese Methode wird von dem CommandManager aufgerufen und überprüft, ob das Command ausgeführt werden kann.
+        /// Wenn in dem Func-Delegaten canExecuteEvaluator null steht, wird true zurückgegeben.
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            if (this.canExecuteEvaluator == null)
+                return true;
+            
+            bool result = this.canExecuteEvaluator.Invoke();
+            return result;
+        }
+
+        /// <summary>
+        /// Diese Methode wird von der WPF aufgerufen, insofern CanExecute true zurückgiebt.
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            this.methodToExecute.Invoke();
+        }
+
+    }
+}
